7993cba13f7626b4c265e1ecd8c0c22a5ecef527
Rodrigo Liberti <liberti1991@gmail.com>
2024-09-27 21:26:25 -0300

    Feat: start project

---
+++ src/user/user.controller.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+  @Get()
+  async getAllUsers() {
+    return JSON.stringify({ teste: 'abc' })
+  }
+}
