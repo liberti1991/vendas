f4137d548a17dd33ff4ddd7f2bfcba2576644bb1
Rodrigo Liberti <liberti1991@gmail.com>
2024-09-29 10:58:32 -0300

    feat: services user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) { }
+
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return createUser
+  async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUser)
   }
-  // async getAllUsers() {
-  //   return JSON.stringify({ teste: 'abc' })
-  // }
+
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers()
+  }
 }
